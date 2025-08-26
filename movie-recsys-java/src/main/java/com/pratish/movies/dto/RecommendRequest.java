a1114264be130fbc37a3484ab6055cecabeb2edf
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-08-26 13:34:41 -0400

    Update RecommendRequest.java

---
+++ movie-recsys-java/src/main/java/com/pratish/movies/dto/RecommendRequest.java
@@ -0,0 +1,12 @@
+package com.pratish.movies.dto;
+
+import lombok.*;
+
+/**
+ * Request payload for recommendation endpoints.
+ */
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class RecommendRequest {
+    private Integer userId;
+    private Integer topN; // optional; controller defaults if null/<=0
+}
