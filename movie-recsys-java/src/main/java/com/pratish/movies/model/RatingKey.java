acaf58bf1dcb902ef6592f07a437bb06f14648c5
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-10-02 22:29:21 -0400

    Update RatingKey.java

--- movie-recsys-java/src/main/java/com/pratish/movies/model/RatingKey.java
+++ movie-recsys-java/src/main/java/com/pratish/movies/model/RatingKey.java
@@ -4,10 +4,11 @@ import lombok.*;
 import java.io.Serializable;
 
 /**
- * Composite primary key for Rating (userId + movieId).
+ * Composite primary key for Rating(userId + movieId).
  */
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @EqualsAndHashCode
 public class RatingKey implements Serializable {
     private Integer userId;
     private Integer movieId;
 }
+
