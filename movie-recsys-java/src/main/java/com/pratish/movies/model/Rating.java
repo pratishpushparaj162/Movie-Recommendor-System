9c1243aaaf872e94eb8e7edcb97fa5fa02189e7d
pratishpushparaj162 <pratish.pushparaj16@gmail.com>
2025-10-02 22:29:02 -0400

    Update Rating.java

--- movie-recsys-java/src/main/java/com/pratish/movies/model/Rating.java
+++ movie-recsys-java/src/main/java/com/pratish/movies/model/Rating.java
@@ -6,7 +6,7 @@ public class Rating {
     private final double rating;
 
     public Rating(int userId, int movieId, double rating) {
-        this.userId = userId; this.movieId = movieId; this.rating = rating;
+        this.userId = userId; this.movieId = movieId;  this.rating = rating;
     }
     public int getUserId() { return userId; }
     public int getMovieId() { return movieId; }
